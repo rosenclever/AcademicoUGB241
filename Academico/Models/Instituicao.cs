05ab8b39741b1f860d1c9b5b72d4f6a4accba5b2
3174 <3174@LAB1-PROF>
2024-05-21 21:02:58 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Models/Instituicao.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academico.Models
+{
+    public class Instituicao
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
